5676a19d0a101b5bd4ec4ae10e9aea8575c4bbea
shivaduke28 <shivaduke28@gmail.com>
2025-04-11 20:00:33 +0900

    show triangle

--- src/components/WebGLCanvas.tsx
+++ src/components/WebGLCanvas.tsx
@@ -19,9 +19,72 @@ const WebGLCanvas: React.FC<WebGLCanvasProps> = ({ width = 800, height = 600 }) 
             return;
         }
 
-        gl.clearColor(1, 0, 0, 1);
+        gl.clearColor(0, 0, 0, 1);
         gl.clear(gl.COLOR_BUFFER_BIT);
-    })
+
+        const vertexShaderSource = `
+        attribute vec4 position;
+        void main() {
+            gl_Position = position;
+        }
+        `;
+
+        const fragmentShaderSource = `
+        void main() {
+          gl_FragColor = vec4(1.0, 0.0, 0.0,1.0);
+        }
+        `;
+
+        const createShader = (type: number, source: string): WebGLShader | null => {
+            const shader = gl.createShader(type);
+            if (!shader) return null;
+            gl.shaderSource(shader, source);
+            gl.compileShader(shader);
+
+            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+                console.error(gl.getShaderInfoLog(shader));
+                gl.deleteShader(shader);
+                return null;
+            }
+
+            return shader;
+        };
+
+        const vertexShader = createShader(gl.VERTEX_SHADER, vertexShaderSource);
+        const fragmentShader = createShader(gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+        if (!vertexShader || !fragmentShader) return;
+        const program = gl.createProgram();
+        if (!program) return;
+
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            console.error(gl.getProgramInfoLog(program));
+            return;
+        }
+
+        gl.useProgram(program);
+
+        const vertices = new Float32Array([
+            0.0, 1.0,
+            -1.0, -1.0,
+            1.0, -1.0,
+        ]);
+
+        const buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        // 何？
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+        const positionLocation = gl.getAttribLocation(program, 'position');
+        gl.enableVertexAttribArray(positionLocation);
+        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+    }, []);
 
     return (
         <canvas ref={canvasRef} width={width} height={height} />
