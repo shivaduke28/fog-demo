9c2bca235e428f22ae0feef63b6b670852249f79
shivaduke28 <shivaduke28@gmail.com>
2025-04-13 15:06:04 +0900

    delete react text

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ function App() {
   return (
     <>
       <h1>Fog Demo</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <WebGLCanvas width={800} height={600} />
     </>
   )
