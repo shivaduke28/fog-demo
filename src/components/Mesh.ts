dfb31b8450f05b171e4ec8ba2643c06434cb49a4
shivaduke28 <shivaduke28@gmail.com>
2025-04-13 14:37:14 +0900

    class -> type

--- src/components/Mesh.ts
+++ src/components/Mesh.ts
@@ -1,7 +1,7 @@
 import { quat, vec3, vec4 } from "gl-matrix";
 import { Geometry } from "./Geometry";
 
-export interface Mesh {
+export type Mesh = {
     geomtry: Geometry;
     position: vec3;
     rotation: quat
@@ -9,22 +9,12 @@ export interface Mesh {
     color: vec4;
 }
 
-class MeshImpl implements Mesh {
-    geomtry: Geometry;
-    position: vec3;
-    rotation: quat;
-    scale: vec3;
-    color: vec4;
-
-    constructor(geometry: Geometry) {
-        this.geomtry = geometry;
-        this.position = vec3.fromValues(0, 0, 0);
-        this.rotation = quat.create();
-        this.scale = vec3.fromValues(1, 1, 1);
-        this.color = vec4.fromValues(1, 1, 1, 1);
-    }
-}
-
 export function createMesh(geometry: Geometry): Mesh {
-    return new MeshImpl(geometry);
+    return {
+        geomtry: geometry,
+        position: vec3.fromValues(0, 0, 0),
+        rotation: quat.create(),
+        scale: vec3.fromValues(1, 1, 1),
+        color: vec4.fromValues(1, 1, 1, 1),
+    }
 };
